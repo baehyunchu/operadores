bbaa06f279a7cd24f23c81eaebdaddc68d95d19e
Julia Fracola <134508829+baehyunchu@users.noreply.github.com>
2023-09-20 07:54:49 -0300

    Add files via upload

---
+++ operadores/operadores.js
@@ -0,0 +1,12 @@
+
+    function CalculaValores(){
+        var s1 = document.getElementById("valor1").value;
+        var s2 = document.getElementById("valor2").value;
+        var s3 = parseInt(s1) + parseInt(s2);
+
+        alert(s3);
+
+
+
+
+    }
